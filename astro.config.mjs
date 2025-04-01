54a559fb7bb350e2daf08da0cbb500553dd7ec89
Leo Araya <larayavrs@gmail.com>
2025-04-01 16:56:53 -0300

    chore(dep): astro-icon for icon supporting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
+import icon from "astro-icon";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
 });
