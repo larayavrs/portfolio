d0bfdc81b715d30126e7e3fbf83d546d90ac0906
Leo Araya <larayavrs@gmail.com>
2025-04-03 02:18:27 -0300

    feat(ui): add MobileMenu component for responsive navigation

---
+++ src/components/ui/mobile-menu.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { navigation } from "@/constants";
+import { Menu } from "lucide-react";
+
+export const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    const handleViewTransitStart = () => {
+      setIsOpen(false);
+    };
+    document.addEventListener("astro:before-swap", handleViewTransitStart);
+    return () => {
+      document.removeEventListener("astro:before-swap", handleViewTransitStart);
+    };
+  }, []);
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={(val) => setIsOpen(val)}>
+      <DropdownMenuTrigger asChild onClick={() => setIsOpen((val) => !val)}>
+        <Button
+          className="md:hidden"
+          variant="outline"
+          size="icon"
+          title="Menu"
+        >
+          <Menu className="h-5 w-5" />
+          <span className="sr-only">Mostrar menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="bg-background">
+        {navigation.map((item) => (
+          <DropdownMenuItem key={item.href} asChild>
+            <a
+              href={item.href}
+              className="w-full text-lg font-medium capitalize"
+              onClick={() => setIsOpen(false)}
+            >
+              {item.label}
+            </a>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
