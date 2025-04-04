a8dd67aa602251631a9f8b717375ce18df51e6b5
Leo Araya <larayavrs@gmail.com>
2025-04-04 01:19:57 -0300

    feat(content): define collections for posts and projects with validation schema

---
+++ src/content.ts
@@ -0,0 +1,34 @@
+import { glob } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
+
+const post = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/posts" }),
+  schema: ({ image }) => {
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      date: z.coerce.date(),
+      image: image().optional(),
+      tags: z.array(z.string()).optional(),
+      draft: z.boolean().optional(),
+      featured: z.boolean().optional(),
+    });
+  },
+});
+
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects" }),
+  schema: ({ image }) => {
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      tags: z.array(z.string()).optional(),
+      image: image(),
+      link: z.string().url(),
+      started: z.coerce.date().optional(),
+      finished: z.coerce.date().optional(),
+    });
+  },
+});
+
+export const collections = { post, projects };
