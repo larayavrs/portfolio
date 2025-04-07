08aadcc11a14e38afea78ff3cac83561f0a5d6d1
Leo Araya <larayavrs@gmail.com>
2025-04-07 19:00:16 -0400

    feat(data): implement getAdjacentPosts function and update getAllProjects sorting

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,3 +13,25 @@ export async function getRecentPosts(
   const posts = await getAllPosts();
   return posts.slice(0, count);
 }
+
+export async function getAdjacentPosts(currentId: string): Promise<{
+  prev: CollectionEntry<"post"> | null;
+  next: CollectionEntry<"post"> | null;
+}> {
+  const posts = await getAllPosts();
+  const current = posts.findIndex((post) => post.id === currentId);
+  if (current === -1) return { prev: null, next: null };
+  return {
+    next: current > 0 ? posts[current - 1] : null,
+    prev: current < posts.length - 1 ? posts[current + 1] : null,
+  };
+}
+
+export async function getAllProjects(): Promise<CollectionEntry<"projects">[]> {
+  const projects = await getCollection("projects");
+  return projects.sort((a, b) => {
+    const adate = a.data.started?.getTime() || 0;
+    const bdate = b.data.started?.getTime() || 0;
+    return bdate - adate;
+  });
+}
