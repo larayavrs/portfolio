180b92c2eea90bd9b8a49eb4344fa11f6bb235ce
Leo Araya <larayavrs@gmail.com>
2025-04-07 22:01:15 -0400

    feat(utils): add formatDate and getReadingTime functions for date formatting and reading time estimation

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: Date) {
+  return Intl.DateTimeFormat("es-ES", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(date);
+}
+
+export function getReadingTime(html: string) {
+  const text = html.replace(/<[^>]+>/g, "");
+  const words = text.split(/\s+/).length;
+  // Assuming an average reading speed of 200 words per minute (adjust as needed)
+  const readingTime = (words / 200 + 1).toFixed();
+  return `${readingTime} minutos de lectura`;
+}
