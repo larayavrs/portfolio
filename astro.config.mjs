8710e31b12c38aac4cf886b70131114fc84b6235
Leo Araya <larayavrs@gmail.com>
2025-04-09 19:39:40 -0400

    feat(astro): some important details to astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,102 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+import expressiveCode from "astro-expressive-code";
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypeKatex from "rehype-katex";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeDocument from "rehype-document";
+
+import remarkEmoji from "remark-emoji";
+import remarkMath from "remark-math";
+import remarkSectionize from "remark-sectionize";
+
+import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), sitemap(), react(), icon()],
+  integrations: [
+    expressiveCode({
+      themes: ["github-light", "github-dark"],
+      plugins: [pluginCollapsibleSections(), pluginLineNumbers()],
+      useDarkModeMediaQuery: false,
+      themeCssSelector: (theme) => `.${theme.name.split("-")[1]}`,
+      defaultProps: {
+        wrap: true,
+        collapseStyle: "collapsible-auto",
+        overridesByLang: {
+          "ansi,bat,bash,batch,cmd,console,powershell,ps,ps1,psd1,psm1,sh,shell,shellscript,shellsession,text,zsh":
+            {
+              showLineNumbers: false,
+            },
+        },
+      },
+      styleOverrides: {
+        borderColor: "var(--border)",
+        codeFontFamily: "var(--font-mono)",
+        codeBackground:
+          "color-mix(in oklab, var(--secondary) 25%, transparent)",
+        frames: {
+          editorActiveTabForeground: "var(--muted-foreground)",
+          editorActiveTabBackground:
+            "color-mix(in oklab, var(--secondary) 25%, transparent)",
+          editorActiveTabIndicatorBottomColor: "transparent",
+          editorActiveTabIndicatorTopColor: "transparent",
+          editorTabBarBackground: "transparent",
+          editorTabBarBorderBottomColor: "transparent",
+          frameBoxShadowCssValue: "none",
+          terminalBackground:
+            "color-mix(in oklab, var(--secondary) 25%, transparent)",
+          terminalTitlebarBackground: "transparent",
+          terminalTitlebarBorderBottomColor: "transparent",
+          terminalTitlebarForeground: "var(--muted-foreground)",
+        },
+        lineNumbers: {
+          foreground: "var(--muted-foreground)",
+        },
+        uiFontFamily: "var(--font-sans)",
+      },
+    }),
+    mdx(),
+    sitemap(),
+    react(),
+    icon(),
+  ],
+  devToolbar: {
+    enabled: false,
+  },
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypeDocument,
+        {
+          css: "https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css",
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["nofollow", "noopener", "noreferrer"],
+        },
+      ],
+      rehypeHeadingIds,
+      rehypeKatex,
+      [
+        rehypePrettyCode,
+        {
+          theme: {
+            light: "github-light",
+            dark: "github-dark",
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [remarkEmoji, remarkMath, remarkSectionize],
+  },
 });
