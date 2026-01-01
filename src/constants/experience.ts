ad0fa7a9d9f5204f85f135b2e4127e948ffe6ad7
Leo Araya <larayavrs@gmail.com>
2026-01-01 12:48:05 -0300

    feat: add EXPERIENCE constant to showcase professional background

---
+++ src/constants/experience.ts
@@ -0,0 +1,21 @@
+export const EXPERIENCE = [
+  {
+    company: "Mister Web",
+    position: "Desarrollador Backend",
+    start: "2021",
+    end: "2024",
+    jobs: [
+      "Participé en la creación de múltiples proyectos, tales como: e-commerces, aplicaciones de multiuso y automatizaciones",
+    ],
+  },
+  {
+    company: "ARAMCO Marga Marga",
+    position: "Servicios de TI freelance",
+    start: 2022,
+    end: 2025,
+    jobs: [
+      "Mantención e instalación de sistemas operativos y cloud computing",
+      "Solucioné, optimizé y actualizé sistemas y dispositivos de la empresa junto a conexiones y temas a la nube",
+    ],
+  },
+];
